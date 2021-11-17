f9b7120eab263971b6be9cf53581afbbb3d5756d
Greg Rushton <grushton@broadinstitute.org>
2021-11-17 11:49:29 -0500

    better and more complete listing

--- src/main.rs
+++ src/main.rs
@@ -7,28 +7,18 @@ fn main() -> () {
     let config: Config = config::parse_config("./config.yaml").unwrap();
     let api_key: String = config.api_key;
 
-    println!("Listing Routes");
     let routes = routes::query_routes(false, Some(api_key.clone()));
     routes.unwrap().data.iter().for_each(|data| {
-        println!("{}", data)
-    });
-    
-    println!("Listing Stops for Red Line");
-    let red_line = routes::query_route_stops(false, Some(api_key.clone()), "Red".to_string());
-    red_line.unwrap().data.iter().for_each(|data| {
-        println!("{}", data)
-    });
-
-    println!("Listing Stops for Orange Line");
-    let orange_line = routes::query_route_stops(false, Some(api_key.clone()), "Orange".to_string());
-    orange_line.unwrap().data.iter().for_each(|data| {
-        println!("{}", data)
-    });
-
-    println!("Listing Stops for all Green Lines");
-    let green_line = routes::query_route_stops(false, Some(api_key.clone()), "Green-B,Green-C,Green-D,Green-E".to_string());
-    green_line.unwrap().data.iter().for_each(|data| {
-        println!("{}", data)
+        let long_name = &data.attributes.long_name;
+        let id = &data.id;
+        let dashes = std::iter::repeat("-").take(long_name.chars().count()).collect::<String>();
+        println!("-------------------{}", dashes);
+        println!("Listing Stops for: {}", long_name);
+        println!("-------------------{}", dashes);
+        let line = routes::query_route_stops(false, Some(api_key.clone()), id.clone());
+        line.unwrap().data.iter().for_each(|data| {
+            println!("{}", data)
+        });
     });
 
 }
